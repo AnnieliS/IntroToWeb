bc40d18c613b26127ae5409f76342879fd4513f2
Annieli Siegel <duddoodler@gmail.com>
2019-05-20 15:22:08 +0300

    competition form

--- exercises/Form/form_main.php
+++ exercises/Form/form_main.php
@@ -7,45 +7,41 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="includes/styles.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Finger+Paint|Fredericka+the+Great|Monoton" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Raleway|Electrolize&display=swap" rel="stylesheet">
     <title></title>
 
 </head>
 
-<body>
-        <?php
+<body id="layout2">
 
-        $name = $_GET["name"];
-        $Ulthar = $_GET["city1"];
-        $Skai = $_GET["river"];
-        $fire = $_GET["setting"];
-        $see = $_GET["Sense"];
-        $Aegyptus = $_GET["god"];
-        $Meroe = $_GET["city2"];
-        $Ophir = $_GET["city3"];
-        $Africa = $_GET["continent"];
-        $speaks  = $_GET["sense"];
-        $ancient = $_GET["adjective"];
+    <div id="bg">
+        <div id="flashlight"></div>
+    </div>
+    <div class="floating">
+        <h1>
+            The Cats of Ulthar <br>
+        </h1>
+        <h3>
 
-
-
-        echo "The Cats of Ulthar";
-        echo "Reimagined by " .name . "...";
-        echo "It is said that in " . $Ulthar .  "which lies beyond the river " . $Skai . "no man may kill a cat;";
-        echo "And this I can verily believe as I gaze upon him who sitteth purring before the " .  $fire .".";
-        echo "For the cat is cryptic, and close to strange things which men cannot  " . $see. ".";
-        echo  "He is the soul of antique " . $Aegyptus . ", and bearer of tales from forgotten cities in " . $Meroe . "and " .  $Ophir .".";
-        echo  "He is the kin of the jungles lords, and heir to the secrets of hoary and sinister " . $Africa .".";
-        echo   "The Sphinx is his cousin, and he " . $speaks . "her language;";
-        echo    "but he is more " . $ancient . "than the Sphinx, and remembers that which she hath forgotten.";
-
-        ?>
+            <i>Reimagined by <?php echo $_GET['name']; ?> . </i> <br>
+            It is said that in <?php echo $_GET['city1']; ?> which lies beyond the river <?php $_GET['river']; ?> no man may kill a cat.<br>
+            And this I can verily believe as I gaze upon him who sitteth purring before the <?php echo $_GET['setting']; ?>. <br>
+            For the cat is cryptic, and close to strange things which men cannot <?php echo $_GET['Sense']; ?>. <br>
+            He is the soul of antique <?php echo $_GET['god']; ?> and bearer of tales from forgotten cities in- <?php foreach ($_GET['city2'] as $value) {
+                                                                                                                    echo  $value . ".";
+                                                                                                                } ?> <br>
+            He is the kin of the jungles lords, and heir to the secrets of hoary and sinister <?php echo $_GET['continent']; ?> . <br>
+            The Sphinx is his cousin, and he <?php echo $_GET['speaks']; ?> her language,<br>
+            but he is more <?php echo $_GET['adjective']; ?> than the Sphinx, and remembers that which she hath forgotten.
 
 
+        </h3>
 
-        <!--BACKGROUND-->
-        <img id="cat" src="http://awiclass.monoame.com/catpic/cat_lefttop.png">
-        <script src="includes/main.js"></script>
+    </div>
+
+    <!--BACKGROUND-->
+    <img id="cat" src="http://awiclass.monoame.com/catpic/cat_lefttop.png">
+    <script src="includes/main.js"></script>
 
 
 </body>
--- exercises/Form/includes/main.js
+++ exercises/Form/includes/main.js
@@ -1,3 +1,5 @@
+
+/*Cat Look To Mouse*/
 $(window).mousemove(function(evt){
     var x = evt.pageX;
     var y = evt.pageY;
@@ -19,3 +21,20 @@ $(window).mousemove(function(evt){
     
     
   });
+
+  /*Flashlight*/
+  $(document).ready(function(){
+    var fl_w = $('#flashlight').width();
+    var fl_h = $('#flashlight').height();
+    
+    $(window).mousemove(function(e){
+        var x1 = e.pageX;
+        var x2 = $(window).scrollLeft();
+        console.log("x2 = " + x2);
+        var y = e.pageY;
+        var mX = x1 - x2 - fl_w / 2;
+        console.log("mX = " + mX);
+        var mY = y - fl_h / 2;
+        $('#flashlight').css({top: mY+"px", left: mX+"px", cursor: "none"});
+    });
+});
