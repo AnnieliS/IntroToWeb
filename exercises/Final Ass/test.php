c9659360ae88e76c3217b83b5a7ff684e452ff50
or eitan <oreitan@gmail.com>
2019-05-24 11:36:17 +0300

    lightbox with ajax test

---
+++ exercises/Final Ass/test.php
@@ -0,0 +1,10 @@
+<div id="one" class="content">
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque auctor nibh eu nibh scelerisque malesuada. Morbi mollis eleifend turpis. Mauris consequat convallis volutpat. Integer quis erat vehicula, molestie nulla vel, sagittis odio. Quisque hendrerit eleifend magna, sit amet dictum odio condimentum a. Proin et ipsum venenatis, elementum sem convallis, pellentesque elit. Mauris congue velit porttitor dui condimentum porttitor. Aenean pretium suscipit ante in imperdiet.
+  </p>
+</div>
+<div id="two" class="content">
+  <p>
+    Nam vitae suscipit metus, eget volutpat quam. Ut et sem nunc. Vivamus erat leo, auctor in neque non, malesuada consectetur neque. Ut ac purus est. Quisque molestie pharetra sem sit amet ornare. Nam eu felis in nisl lacinia iaculis. Pellentesque ut lobortis lacus. Etiam ut eros non dui ultrices imperdiet. 
+  </p>
+</div>
