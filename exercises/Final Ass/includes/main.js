0dd0f280da42826de7ee015083b49f47af9f909c
or eitan <oreitan@gmail.com>
2019-05-24 20:41:58 +0300

    js lightbox test work

--- exercises/Final Ass/includes/main.js
+++ exercises/Final Ass/includes/main.js
@@ -1,1 +1,10 @@
-
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) 
+  var recipient = button.data('whatever')
+  $.get('test.php?reg_un='+recipient,function(data){
+    modal.find('.modal-body').html(data) 
+  })
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+ 
+})
--- exercises/Final Ass/test.php
+++ exercises/Final Ass/test.php
@@ -1,10 +1,20 @@
-<div id="one" class="content">
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque auctor nibh eu nibh scelerisque malesuada. Morbi mollis eleifend turpis. Mauris consequat convallis volutpat. Integer quis erat vehicula, molestie nulla vel, sagittis odio. Quisque hendrerit eleifend magna, sit amet dictum odio condimentum a. Proin et ipsum venenatis, elementum sem convallis, pellentesque elit. Mauris congue velit porttitor dui condimentum porttitor. Aenean pretium suscipit ante in imperdiet.
-  </p>
-</div>
-<div id="two" class="content">
-  <p>
-    Nam vitae suscipit metus, eget volutpat quam. Ut et sem nunc. Vivamus erat leo, auctor in neque non, malesuada consectetur neque. Ut ac purus est. Quisque molestie pharetra sem sit amet ornare. Nam eu felis in nisl lacinia iaculis. Pellentesque ut lobortis lacus. Etiam ut eros non dui ultrices imperdiet. 
-  </p>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+        
+</head>
+<body>
+<section>
+            Welcome
+            <?php
+                $un = $_GET["reg_un"];
+                if ($un == "or" )
+                    echo "<h2>Good morning user: " . $un . "</h2>";
+                else
+                    echo "<h2>you shell not pass</h2>";
+                ?>
+        </section>
+
+
+</body>
+</html>
