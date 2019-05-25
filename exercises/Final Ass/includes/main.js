0e76a6e6a49d3da428b2286a17d50d905fc0b5c8
Annieli Siegel <duddoodler@gmail.com>
2019-05-25 15:28:16 +0300

    RevengePage Fix1

--- exercises/Final Ass/includes/main.js
+++ exercises/Final Ass/includes/main.js
@@ -1,13 +1,13 @@
 /*-----Lighbox Script------*/
 
 $('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) 
+    var button = $(event.relatedTarget)
     var recipient = button.data('whatever')
-    $.get('virus.php?v_num='+recipient,function(data){
-      console.log(data)
-      modal.find('.modal-body').html(data) 
+    $.get('virus.php?v_num=' + recipient, function (data) {
+        console.log(data)
+        modal.find('.modal-body').html(data)
     })
     var modal = $(this)
     modal.find('.modal-title').text('New message to ' + recipient)
-   
-  })
+
+})
