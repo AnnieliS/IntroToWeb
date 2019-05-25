7f0fabc11eb6df45b7d7eafbeb9b9393c90cd04e
Annieli Siegel <duddoodler@gmail.com>
2019-05-25 14:04:44 +0300

    Main categories + revenge page start

--- exercises/Final Ass/includes/main.js
+++ exercises/Final Ass/includes/main.js
@@ -1,1 +1,13 @@
+/*-----Lighbox Script------*/
 
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) 
+    var recipient = button.data('whatever')
+    $.get('virus.php?v_num='+recipient,function(data){
+      console.log(data)
+      modal.find('.modal-body').html(data) 
+    })
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+   
+  })
