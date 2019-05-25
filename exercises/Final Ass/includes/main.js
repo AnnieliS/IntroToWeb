b62c9df8f8071efb4ac34effcb1f5b6a5b6d9656
or eitan <oreitan@gmail.com>
2019-05-25 10:36:34 +0300

    buy now basic

--- exercises/Final Ass/includes/main.js
+++ exercises/Final Ass/includes/main.js
@@ -1,7 +1,8 @@
 $('#exampleModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) 
   var recipient = button.data('whatever')
-  $.get('test.php?reg_un='+recipient,function(data){
+  $.get('virus.php?v_num='+recipient,function(data){
+    console.log(data)
     modal.find('.modal-body').html(data) 
   })
   var modal = $(this)
---
+++ exercises/Final Ass/virus.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+        
+</head>
+<body>
+<section>
+            Welcome
+            <?php
+                $virus_num = $_GET["v_num"];
+                switch($virus_num) {
+                    case "1":
+                        echo "<p>1</p>";
+                        break;
+                    
+                    case "2":
+                        echo "<p>2</p>";
+                        break;
+
+                    case "3":
+                        echo "<p>3</p>";
+                        break;
+
+                    case "4":
+                        echo "<p>4</p>";
+                        break;
+                    
+                    case "5":
+                        echo "<p>5</p>";
+                        break;
+
+                    case "6":
+                        echo "<p>6</p>";
+                        break;
+
+                    case "7":
+                        echo "<p>7</p>";
+                        break;
+
+                    case "8":
+                        echo "<p>8</p>";
+                        break;
+
+                    case "9":
+                        echo "<p>9</p>";
+                        break;
+
+                    case "10":
+                        echo "<p>10</p>";
+                        break;
+                }   
+                
+                ?>
+        </section>
+
+
+</body>
+</html>
